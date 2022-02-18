b83018d9f73f33f8aee8c1e4678e1278ee4a8682
DavideSegullo <davidesegullo@gmail.com>
2022-02-18 13:31:26 +0100

    Deploy Version 1.4.1 (#76)
    * fix: :bug: fix gamm pool balance
    
    * fix: :bug: fix unknown ibc balance

--- src/services/cosmos.ts
+++ src/services/cosmos.ts
@@ -256,7 +256,7 @@ export const getBalances = async (address: string, validatorsDictionary: { [key:
   try {
     const balancesResponse = await queryAutoPaginate<Coin>(`cosmos/bank/v1beta1/balances/${address}`);
 
-    const balances = balancesResponse ? balancesResponse : [];
+    const balances = balancesResponse ? balancesResponse.filter(el => !el.denom.includes('gamm/pool')) : [];
 
     const coins = await Promise.all(
       balances.map((balance) => {
@@ -309,8 +309,12 @@ export const getBalances = async (address: string, validatorsDictionary: { [key:
       } else {
         const response = await searchSymbolDetails(coin.denom);
         const symbol = response.data.symbol;
-        const responseName = await searchTokenDetails(symbol);
-        const token = responseName.data.find((el) => el.symbol === symbol);
+        let token;
+
+        if (symbol !== '') {
+          const responseName = await searchTokenDetails(symbol);
+          token = responseName.data.find((el) => el.symbol === symbol);
+        }
 
         mappedCoins.push(
           balanceReducer(coin, delegations, undelegations, token ? token.name : symbol, symbol),
