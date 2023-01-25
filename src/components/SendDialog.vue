c0e152f4c83e355cb6de08b0402c9a9290bf35cc
angelorc <angelo@rcstart.it>
2023-01-25 18:58:18 +0100

    fix: send fantokens now calculate the correct amt

--- src/components/SendDialog.vue
+++ src/components/SendDialog.vue
@@ -2,16 +2,9 @@
   <q-dialog ref="dialogRef" @hide="onDialogHide">
     <q-card class="body">
       <div class="dialog-header row items-center justify-between full-width">
-        <h2 class="title text-body-large text-white q-my-none" v-if="!error && ! success">{{ $t('actions.send') }}</h2>
+        <h2 class="title text-body-large text-white q-my-none" v-if="!error && !success">{{ $t('actions.send') }}</h2>
 
-        <q-btn
-          unelevated
-          rounded
-          text-color="white"
-          class="close no-hoverable q-ml-auto"
-          padding="2px"
-          @click="close"
-        >
+        <q-btn unelevated rounded text-color="white" class="close no-hoverable q-ml-auto" padding="2px" @click="close">
           <label class="text-body4 text-uppercase no-pointer-events">{{ $t('actions.close') }}</label>
           <q-icon class="close-icon" name="svguse:icons.svg#close|0 0 12 12" size="10px" />
         </q-btn>
@@ -20,20 +13,13 @@
       <template v-if="!error">
         <q-form class="col column items-center fit no-wrap" @submit="onSubmit" v-if="!success">
           <div class="field-block column full-width">
-            <label class="field-label text-uppercase text-primary text-h6 text-weight-medium">{{ $t('general.sendTo') }}</label>
+            <label class="field-label text-uppercase text-primary text-h6 text-weight-medium">{{
+              $t('general.sendTo')
+            }}</label>
 
-            <q-input
-              v-model="to"
-              color="transparent-white"
-              label-color="accent-5"
-              bg-color="transparent-white"
-              round
-              standout
-              no-error-icon
-              hide-bottom-space
-              class="full-width large"
-              :rules="[val => !!val || $t('errors.required'), val => isValidAddress(val) || $t('actions.invalidAddress')]"
-            >
+            <q-input v-model="to" color="transparent-white" label-color="accent-5" bg-color="transparent-white" round
+              standout no-error-icon hide-bottom-space class="full-width large"
+              :rules="[val => !!val || $t('errors.required'), val => isValidAddress(val) || $t('actions.invalidAddress')]">
               <template v-slot:append>
                 <q-icon name="svguse:icons.svg#anchor" size="16px" color="gray3" />
               </template>
@@ -41,28 +27,21 @@
           </div>
 
           <div class="field-block column full-width">
-            <label class="field-label text-uppercase text-primary text-h6 text-weight-medium">{{ $t('general.amount') }}</label>
+            <label class="field-label text-uppercase text-primary text-h6 text-weight-medium">{{
+              $t('general.amount')
+            }}</label>
 
-            <q-input
-              v-model="amount"
-              color="transparent-white"
-              label-color="accent-5"
-              bg-color="transparent-white"
-              round
-              standout
-              no-error-icon
-              hide-bottom-space
-              class="quantity-input full-width large"
-              :rules="[
+            <q-input v-model="amount" color="transparent-white" label-color="accent-5" bg-color="transparent-white"
+              round standout no-error-icon hide-bottom-space class="quantity-input full-width large" :rules="[
                 val => !!val || $t('errors.required'),
                 val => !isNaN(val) || $t('errors.nan'),
                 val => gtnZero(val) || $t('errors.gtnZero'),
                 val => compareBalance(val, availableCoins) || $t('errors.balanceMissing'),
                 val => !isNegative(val) || $t('errors.negative')
-              ]"
-            >
+              ]">
               <template v-slot:append>
-                <q-btn @click="amount = getMaxAmount()" class="max-btn btn-super-extra-small text-body3" rounded unelevated color="accent-2" text-color="white" padding="4px 7px 3px">
+                <q-btn @click="amount = getMaxAmount()" class="max-btn btn-super-extra-small text-body3" rounded
+                  unelevated color="accent-2" text-color="white" padding="4px 7px 3px">
                   {{ $t('actions.max') }}
                 </q-btn>
                 <label class="text-body2 text-primary" v-if="!denom">{{ network.stakingDenom }}</label>
@@ -71,62 +50,37 @@
             </q-input>
 
             <p class="text-body2 text-primary q-px-sm q-mt-sm q-mb-none">
-              {{ $t('general.availableCoins', { amount: denom ? availableCoins.toFixed() : availableCoins.toFormat() }) }}
+              {{ $t('general.availableCoins', { amount: denom ? availableCoins.toFixed() : availableCoins.toFormat() })
+              }}
               <span class="text-uppercase" v-if="!denom">{{ network.stakingDenom }}</span>
               <span class="text-uppercase" v-else-if="symbol">{{ symbol }}</span>
             </p>
           </div>
 
           <div class="field-block column full-width justify-start items-start">
-            <q-btn
-              @click="showAdvanced = !showAdvanced"
-              class="no-hoverable col-auto"
-              toggle-color="primary"
-              flat
-              unelevated
-              :ripple="false"
-              text-color="accent"
-              toggle-text-color="white"
-              padding="0 0 0 11px"
-            >{{ !showAdvanced ? $t('actions.showAdvanced') : $t('actions.hideAdvanced') }}</q-btn>
+            <q-btn @click="showAdvanced = !showAdvanced" class="no-hoverable col-auto" toggle-color="primary" flat
+              unelevated :ripple="false" text-color="accent" toggle-text-color="white" padding="0 0 0 11px">{{
+              !showAdvanced ? $t('actions.showAdvanced') : $t('actions.hideAdvanced') }}</q-btn>
           </div>
 
-          <transition
-            enter-active-class="animated fadeIn"
-            leave-active-class="animated fadeOut"
-            mode="out-in"
-            appear
-          >
+          <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" mode="out-in" appear>
             <div class="field-block column full-width" v-if="showAdvanced">
-              <label class="field-label text-uppercase text-primary text-h6 text-weight-medium">{{ $t('general.note') }}</label>
+              <label class="field-label text-uppercase text-primary text-h6 text-weight-medium">{{
+                $t('general.note')
+              }}</label>
 
-              <q-input
-                v-model="memo"
-                color="transparent-white"
-                label-color="accent-5"
-                bg-color="transparent-white"
-                round
-                standout
-                no-error-icon
-                hide-bottom-space
-                class="full-width large"
-              />
+              <q-input v-model="memo" color="transparent-white" label-color="accent-5" bg-color="transparent-white"
+                round standout no-error-icon hide-bottom-space class="full-width large" />
             </div>
           </transition>
 
           <div class="btns full-width items-center justify-end q-mt-auto">
-            <q-btn
-              unelevated
-              rounded
-              text-color="white"
-              class="close no-hoverable"
-              padding="2px"
-              @click="close"
-            >
+            <q-btn unelevated rounded text-color="white" class="close no-hoverable" padding="2px" @click="close">
               <label class="text-h5 text-capitalize no-pointer-events">{{ $t('actions.cancel') }}</label>
             </q-btn>
 
-            <q-btn type="submit" class="submit btn-medium text-h5" rounded unelevated color="accent-2" text-color="white" padding="15px 20px 14px" :loading="loading">
+            <q-btn type="submit" class="submit btn-medium text-h5" rounded unelevated color="accent-2"
+              text-color="white" padding="15px 20px 14px" :loading="loading">
               {{ $t('actions.send') }}
             </q-btn>
           </div>
@@ -135,11 +89,17 @@
         <div class="success col column fit" v-else>
           <q-icon class="success-icon" name="svguse:icons.svg#check|0 0 70 70" size="64px" color="positive" />
 
-          <h3 class="text-body-extra-large text-white text-weight-medium q-mt-none q-mb-sm text-center">{{ $t('success.title') }}</h3>
+          <h3 class="text-body-extra-large text-white text-weight-medium q-mt-none q-mb-sm text-center">{{
+            $t('success.title')
+          }}</h3>
 
-          <p class="text-h4 text-half-transparent-white text-center">{{ $t('success.send', { symbol: symbol ? symbol : network.stakingDenom }) }}</p>
+          <p class="text-h4 text-half-transparent-white text-center">{{
+        $t('success.send', { symbol: symbol ? symbol:
+          network.stakingDenom }) }}</p>
 
-          <q-btn @click="close" type="a" target="_blank" :href="network.explorerURL + 'txs/' + hash" class="transaction-btn q-mx-auto btn-medium text-body2 text-untransform text-weight-medium" rounded unelevated color="accent-gradient" text-color="white" padding="15px 20px 14px">
+          <q-btn @click="close" type="a" target="_blank" :href="network.explorerURL + 'txs/' + hash"
+            class="transaction-btn q-mx-auto btn-medium text-body2 text-untransform text-weight-medium" rounded
+            unelevated color="accent-gradient" text-color="white" padding="15px 20px 14px">
             {{ $t('actions.transactions') }}
           </q-btn>
         </div>
@@ -148,7 +108,9 @@
       <div class="success col column fit" v-else>
         <q-icon class="success-icon" name="svguse:icons.svg#error-outlined|0 0 70 70" size="64px" color="negative" />
 
-        <h3 class="text-body-extra-large text-white text-weight-medium q-mt-none q-mb-sm text-center">{{ $t('errors.title') }}</h3>
+        <h3 class="text-body-extra-large text-white text-weight-medium q-mt-none q-mb-sm text-center">{{
+          $t('errors.title')
+        }}</h3>
 
         <p class="text-h4 text-half-transparent-white text-center word-break-break-word">{{ error }}</p>
       </div>
@@ -220,13 +182,20 @@ export default defineComponent({
     };
 
     const onSubmit = async () => {
+      let amt = "0"
+      if (props.denom) {
+        amt = new BigNumber(amount.value).multipliedBy(1000000).toFixed(0).toString();
+      } else {
+        amt = new BigNumber(amount.value).toFixed(0).toString();
+      }
+
       try {
         const request = {
           type: MessageTypes.SEND,
           to: to.value,
           memo: memo.value,
           amounts: [{
-            amount: amount.value,
+            amount: amt,
             denom: props.denom ? props.denom : network.value.stakingDenom
           }],
         };
@@ -289,6 +258,7 @@ export default defineComponent({
 .close {
   opacity: 0.4;
 }
+
 .close-icon {
   margin-left: 15px;
 }
